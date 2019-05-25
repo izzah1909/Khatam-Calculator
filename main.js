e1eeed2065ab63a466586d74202a74d1c09c7422
Nurul 'Izzah Zaini <nrlzzhzn@gmail.com>
2019-05-25 16:12:34 +0800

    Added parseInt to integer value, called 2 variables in one function.
    Added if, else, to add message.

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 
 
 function calculateDaysNeeded() {
-    const pagesRead = document.getElementById("pages").value;
+    const pagesRead = parseInt (document.getElementById("pages").value);
     const daysNeeded = 604 / pagesRead;
-    printDaysNeededToCard(daysNeeded);
+    printDaysNeededToCard(daysNeeded, pagesRead);
     
 }
 
-function printDaysNeededToCard(daysNeeded) {
-    document.getElementById("cardContents").innerHTML = daysNeeded;
+function printDaysNeededToCard(daysNeeded, pagesRead) {
+    let message = "If you read " + pagesRead + " page(s) a day, you will complete it in about " + daysNeeded + " day(s). ";
+    if (pagesRead === 0) {
+        message = "Please read the Quran...";
+    } else if (pagesRead < 0) {
+        message = "Error! No negative pages!";
+    } else if (pagesRead > 604){
+        message = "Error! Maximum pages in the Quran is 604 pages!";
+    } else if (daysNeeded < 30) {
+        message += "Keep up the good pace! You will khatam the quran in no time!";
+    } else if (daysNeeded > 30) {
+        message+="Keep on reading the Quran meticulously! You will reach the end one day!"
+    } else if (daysNeeded > 365) {
+        message += "You will khatam the Quran slowly but surely. All your hardwork will be paid off, insyaallah!";
+    }
+
+    
+
+    document.getElementById("cardContents").innerHTML = message;
     
 }
